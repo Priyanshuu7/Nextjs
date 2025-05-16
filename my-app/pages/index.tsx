db4c455e25fba0a647481778f071f0fbe4b659eb
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-05-16 23:38:22 +0530

    tested terminal

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -10,7 +10,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
+// this is updated throigh terminal //
 export default function Home() {
   return (
     <div
