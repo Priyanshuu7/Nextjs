d6c7457bbcfd7a7e6a13984b7b7b34fddfed0856
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-05-19 22:39:44 +0530

    Mock updated

--- my-app/pages/api/hello.ts
+++ my-app/pages/api/hello.ts
@@ -4,7 +4,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 type Data = {
   name: string;
 };
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(
   req: NextApiRequest,
