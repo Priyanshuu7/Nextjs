7ee806705758ba33f9c8096000b4181463d5408e
Priyanshuu7 <rajakpriyanshu581@gmail.com>
2025-05-27 20:33:32 +0530

    Updated security

--- nextjstut/app/dashboard/page.tsx
+++ nextjstut/app/dashboard/page.tsx
@@ -1,7 +1,17 @@
-export default function DashboardRoute(){
-    return(
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server"
+import {redirect} from "next/navigation";
+
+export default async function DashboardRoute() {
+    const {getUser} = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user) {
+        return redirect("/api/auth/register");
+    }
+
+    return (
         <h1>
-            HEloo for dashboard
+            This is User DashBoard
         </h1>
     )
 }
